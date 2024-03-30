e0abb966b0b6a060d2d435f23d2891d043eb8699
Gauravkandel <gauravkandell990@gmail.com>
2024-03-30 23:21:08 +0545

    forth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Wordle-By-GK</title>
     <link rel="stylesheet" href="wordle.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
